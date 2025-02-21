6ce9d1822666fcf4aa8d5176f3c22b829919904b
Karim Khasan <karimhasan3879@gmail.com>
2025-02-22 02:24:21 +0300

    fix: use secure random algorithm

--- src/main/kotlin/ru/kslacker/highload/config/SnowflakeConfig.kt
+++ src/main/kotlin/ru/kslacker/highload/config/SnowflakeConfig.kt
@@ -3,12 +3,12 @@ package ru.kslacker.highload.config
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import ru.kslacker.highload.service.infrastructure.utils.snowflake.Snowflake
-import java.util.concurrent.ThreadLocalRandom
+import java.security.SecureRandom
 
 @Configuration
 class SnowflakeConfig {
     @Bean
     fun snowflake(): Snowflake {
-        return Snowflake(ThreadLocalRandom.current().nextInt(0, Snowflake.MAX_INSTANCE_ID.toInt() + 1).toLong())
+        return Snowflake(SecureRandom().nextInt(0, Snowflake.MAX_INSTANCE_ID.toInt() + 1).toLong())
     }
 }
